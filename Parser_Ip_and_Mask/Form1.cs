13e64f61740e3fa9d060b0efe11fd98b1fe833ac
https://github.com/AntonVin <toshavin@mail.ru>
2023-06-20 15:09:06 +0300

    Добавил хранение директория в реестр

--- Parser_Ip_and_Mask/Form1.cs
+++ Parser_Ip_and_Mask/Form1.cs
@@ -1,8 +1,10 @@
+using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Parser_Ip_and_Mask
 {
@@ -11,16 +13,19 @@ namespace Parser_Ip_and_Mask
         public Form1()
         {
             InitializeComponent();
+            txtPath.Text = RegistrHelper.GetPath();
         }
 
         private void btnStart_Click(object sender, EventArgs e)
-        { 
-            if(!Directory.Exists(txtPath.Text))
+        {
+            if (!Directory.Exists(txtPath.Text))
             {
                 MessageBox.Show("Такой папки не существует","ОШИБКА",MessageBoxButtons.OK,MessageBoxIcon.Error);
                 return;
             }
             Clear();
+            RegistrHelper.SetPath(txtPath.Text);
+
             Extractor.ExtractDataToTable(txtPath.Text,dgvResult,txtLog);
         }
 
---
+++ Parser_Ip_and_Mask/RegistrHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser_Ip_and_Mask
+{
+    public static class RegistrHelper
+    {
+        public static string GetPath()
+        {
+            var key = Registry.CurrentUser.OpenSubKey("ParserIpAndMask");
+            if (key is null)
+                return "укажите путь";
+            var path = key.GetValue("path") as string;
+            key.Close();
+            return path;
+        }
+
+        public static void SetPath(string path)
+        {
+            var key = Registry.CurrentUser.CreateSubKey("ParserIpAndMask");
+            key.SetValue("path", path);
+            key.Close();
+        }
+    }
+}
